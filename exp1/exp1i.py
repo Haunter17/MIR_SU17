9296470cb2a179c59fbe67fbd3bb3df5289dd7f1
CS70 Horse <cstrong@cs.hmc.edu>
2017-05-24 13:30:21 -0700

    added logging device placement to exp1i

--- exp1/exp1i.py
+++ exp1/exp1i.py
@@ -71,7 +71,7 @@ def runNeuralNet(num_features, hidden_layer_size, X_train, y_train, X_test, y_te
 	cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
 	train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
-	sess = tf.InteractiveSession() #config=tf.ConfigProto(log_device_placement=True)
+	sess = tf.InteractiveSession(config=tf.ConfigProto(log_device_placement=True)) #config=tf.ConfigProto(log_device_placement=True)
 
 	correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float64))
