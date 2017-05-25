edc75ec17cfeb75870fcbcb86833101ecd17d0b9
CS70 Horse <cstrong@cs.hmc.edu>
2017-05-25 11:15:19 -0700

    updated experiment for 1k

--- exp1/exp1k.py
+++ exp1/exp1k.py
@@ -122,14 +122,14 @@ def runNeuralNet(num_features, hidden_layer_size, X_train, y_train, X_test, y_te
 our main
 '''
 [X_train, y_train, X_test, y_test] = loadData('taylorswift_smallDataset_71_7.mat')
-numEpochs = 3
+numEpochs = 300
 
 numTrainingSamples = X_train.shape[0]
 
 # leave the testing data the same, downsample the training data
 print("==> Starting Downsampling Tests for exp1c")
 # set the rates we want to test at
-batchSizes = [100, 200, 500, 1000, 2000, 5000, 10000]
+batchSizes = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000]
 trainingAccuracies = []
 testAccuracies = []
 trainingCosts = []
