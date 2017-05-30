1ec7b265924c85fcb50df0a4e728635cc90fe39d
Zhepei Wang <haunter@br005.pvt.bridges.psc.edu>
2017-05-30 01:55:21 -0400

    exp1b with reports and plots #4

--- exp1/exp1b.py
+++ exp1/exp1b.py
@@ -2,6 +2,9 @@ import numpy as np
 import tensorflow as tf
 import h5py
 import time
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 print('==> Experiment 1b')
 filepath = '../taylorswift_out/small.mat'
@@ -83,26 +86,70 @@ batchSize = 1000
 numEpochs = 300
 print_freq = 5
 
+train_acc_list = []
+val_acc_list = []
+train_err_list = []
+val_err_list = []
+
 # benchmark
 t_start = time.time()
 for epoch in range(numEpochs):
     for i in range(0,numTrainingVec,batchSize):
 
         # Batch Data
-        batchEndPoint = min(i+batchSize, numTrainingVec)
+        batchEndPoint = min(i + batchSize, numTrainingVec)
         trainBatchData = X_train[i:batchEndPoint]
         trainBatchLabel = y_train[i:batchEndPoint]
 
+        # train
         train_step.run(feed_dict={x: trainBatchData, y_: trainBatchLabel})
 
     # Print accuracy
     if (epoch + 1) % print_freq == 0:
-        train_accuracy = accuracy.eval(feed_dict={x:trainBatchData, y_: trainBatchLabel})
-        print("epoch: %d, training accuracy %g"%(epoch + 1, train_accuracy))
+        # evaluate accuracy and errors
+        train_acc = accuracy.eval(feed_dict={x:X_train, y_: y_train})
+        train_acc_list.append(train_acc)
+        val_acc = accuracy.eval(feed_dict={x: X_test, y_: y_test})
+        val_acc_list.append(val_acc)
+        train_err = cross_entropy.eval(feed_dict={x: X_train, y_: y_train})
+        train_err_list.append(train_err)
+        val_err = cross_entropy.eval(feed_dict={x: X_test, y_: y_test})
+        val_err_list.append(val_err)      
+        print("-- epoch: %d, training accuracy %g"%(epoch + 1, train_acc))
 
 t_end = time.time()
 print('--Time elapsed for training: {t:.2f} \
     seconds'.format(t = t_end - t_start))
 
-# Validation
-print("test accuracy %g"%accuracy.eval(feed_dict={x: X_test, y_: y_test}))
+# Reports
+print('-- Training accuracy: {:.4f}'.format(train_acc_list[-1]))
+print('-- Validation accuracy: {:.4f}'.format(val_acc_list[-1]))
+print('-- Training error: {:.4E}'.format(train_err_list[-1]))
+print('-- Validation error: {:.4E}'.format(val_err_list[-1]))
+
+# Generating plots
+
+
+print('==> Generating accuracy plot...')
+x_list = range(0, print_freq * len(train_acc_list), print_freq)
+plt.xlabel('Number of epochs')
+plt.ylabel('Accuracy (%)')
+plt.title('Accuracy vs Number of Epochs')
+train_acc_plot, = plt.plot(x_list, train_acc_list, 'bo')
+val_acc_plot, = plt.plot(x_list, val_acc_list, 'ro')
+plt.legend((train_acc_plot, val_acc_plot), ('training', 'validation'), loc='best')
+plt.savefig('exp1b_accuracy.png', format='png')
+plt.close()
+
+print('==> Generating error plot...')
+plt.xlabel('Number of epochs')
+plt.ylabel('Cross-Entropy Error')
+plt.title('Error vs Number of Epochs')
+train_err_plot, = plt.plot(x_list, train_err_list, 'bo')
+val_err_plot, = plt.plot(x_list, val_err_list, 'ro')
+plt.legend((train_err_plot, val_err_plot), ('training', 'validation'), loc='best')
+plt.savefig('exp1b_error.png', format='png')
+plt.close()
+
+print('==> Done.')
+
