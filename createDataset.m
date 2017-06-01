2ed80598afa6806ec9b02f87c061835045ceea02
ttanprasert <ttanprasert@hmc.edu>
2017-06-01 15:40:24 -0700

    overlapping windows for exp2

--- createDataset.m
+++ createDataset.m
@@ -1,23 +1,26 @@
-function [trainingSet,testingSet] = createDataset (Q, label)
+function [trainingSet,testingSet] = createDataset (Q, label, strideSize, windowSize)
 
-% 250 frames/sec
-% Training duration: 12 secs = 966 frames = 2 col
-% Testing duration: 6 secs = 483 frames = 1 col
+% With downsampling rate = 15, we have 16 frames/sec
+% Training duration: 10 secs = 160 frames
+% Testing duration: 5 secs = 80 frames
 trainingVec = [];
 testingVec = [];
-trainingDuration = 2;
-testingDuration = 1;
+trainingDuration = 160;
+testingDuration = 80;
 
 for col=1:trainingDuration+testingDuration: size(Q,2)
-	if col + trainingDuration + testingDuration - 1 >= size(Q, 2)
+    for i=col:strideSize:min(col+trainingDuration-windowSize, size(Q,2)-windowSize+1)
+        trainingVec = cat(2,trainingVec,reshape(Q(:,i:i+windowSize-1),121*windowSize,1));
+    end
+    if col + trainingDuration - 1 >= size(Q, 2)
     	break
     end
-    trainingVec = cat(1, trainingVec, Q(:, col : col + trainingDuration - 1)');
-    testingVec = cat(1, testingVec, ...
-    	Q(:, col + trainingDuration : trainingDuration + testingDuration - 1)');
+    for i=col+trainingDuration+1:strideSize:min(col+trainingDuration+testingDuration-windowSize,size(Q,2)-windowSize+1)
+        testingVec = cat(2,testingVec,reshape(Q(:,i:i+windowSize-1),121*windowSize,1));
+    end
 end
 
-trainingLabel = ones(size(trainingVec,1), 1) * label;
-testingLabel = ones(size(testingVec,1), 1) * label;
-trainingSet = [trainingVec trainingLabel];
-testingSet = [testingVec testingLabel];
+trainingLabel = ones(size(trainingVec',1), 1) * label;
+testingLabel = ones(size(testingVec',1), 1) * label;
+trainingSet = [trainingVec' trainingLabel];
+testingSet = [testingVec' testingLabel];
