f726cb0688bb71569a94e25a78b1725ddb2516c3
ttanprasert <ttanprasert@hmc.edu>
2017-06-08 12:08:09 -0700

    transpose CQT matrix before stacking

--- createDataset.m
+++ createDataset.m
@@ -10,13 +10,13 @@ testingDuration = 80;
 
 for col=1:trainingDuration+testingDuration: size(Q,2)
     for i=col:strideSize:min(col+trainingDuration-windowSize, size(Q,2)-windowSize+1)
-        trainingVec = cat(2,trainingVec,reshape(Q(:,i:i+windowSize-1),121*windowSize,1));
+        trainingVec = cat(2,trainingVec,reshape(Q(:,i:i+windowSize-1)',121*windowSize,1));
     end
     if col + trainingDuration - 1 >= size(Q, 2)
     	break
     end
     for i=col+trainingDuration+1:strideSize:min(col+trainingDuration+testingDuration-windowSize,size(Q,2)-windowSize+1)
-        testingVec = cat(2,testingVec,reshape(Q(:,i:i+windowSize-1),121*windowSize,1));
+        testingVec = cat(2,testingVec,reshape(Q(:,i:i+windowSize-1)',121*windowSize,1));
     end
 end
 
