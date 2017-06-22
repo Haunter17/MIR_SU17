b4ac30bfbfaa4cb04cebfcc72e4da13152e9d7f9
Haunter17 <haunterhacker17@gmail.com>
2017-06-22 15:23:11 -0700

    new lr exp4i

--- exp4/exp4i.py
+++ exp4/exp4i.py
@@ -140,7 +140,7 @@ y_sm = tf.matmul(a_drop, W_sm) + b_sm
 
 cross_entropy = tf.reduce_mean(
 		tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_sm))
-train_step = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer(learning_rate=1e-3).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_sm, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
