f9199bb95b9aa7d110e922f79825308612ccb042
Haunter17 <haunterhacker17@gmail.com>
2017-06-27 17:33:21 -0700

    fixing parallel

--- exp7/benchmark/getHashprintRepresentation.m
+++ exp7/benchmark/getHashprintRepresentation.m
@@ -2,31 +2,25 @@ function [fingerprints] = getHashprintRepresentation(modelFile)
 
 load(modelFile); % loads filelist, parameter, eigvecs, eigvals
 
-
+fingerprints = {};
 fid = fopen(filelist);
-curFileList = '';
-fileIndex = 1;
-
+count = 1;
 curfile = fgetl(fid);
-while ischar(curfile)
-    curFileList{fileIndex} = curfile;
-    curfile = fgetl(fid);
-    fileIndex = fileIndex + 1;
-end
-
-fingerprints = cell(length(curFileList));
 
 %% hashprints for original file
-tic;
-parfor index = 1 : length(curFileList)
-    curfile = curFileList{index};
-    disp(['==> Computing fingerprints on file ',num2str(index),': ',curfile]);
+while ischar(curfile)
+    tic;
+    disp(['==> Computing fingerprints on file ',num2str(count),': ',curfile]);
     Q = computeQSpec(curfile,parameter);
     logQspec = preprocessQspec(Q);
     
     % compute hashprints on original studio track
     fpseq = computeHashprints(logQspec,eigvecs,parameter);
-    fingerprints{index} = fpseq;
+    fingerprints{count} = fpseq;
+    
+    % compare bit match
+    count = count + 1;
+    curfile = fgetl(fid);
+    toc
 end
-toc
 fclose(fid);
