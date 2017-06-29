71fbd79ab900d1c93e50a1aa370c652e6b9d59cb
Haunter17 <haunterhacker17@gmail.com>
2017-06-29 16:59:47 -0700

    AE data init

--- exp7/exp7g/createAEData.m
+++ exp7/exp7g/createAEData.m
@@ -1,6 +1,36 @@
 addpath('../../cqt/');
-filelist = 'something.txt';
-fid = fopen(filelist);
+addpath('../benchmark/');
+
+%% config
+artist = 'taylorswift';
+reflist = strcat('../benchmark/', artist, '_ref.list');
+outdir = strcat('../benchmark/', artist, '_out/');
+param.precomputeCQT = 1;
+param.precomputeCQTdir = outdir;
+savename = strcat(outdir, 'AEdata.mat');
+
+%% param setup
+if isfield(parameter,'m')==0
+    parameter.m=20;
+end
+if isfield(parameter,'tao')==0
+    parameter.tao=1;
+end
+if isfield(parameter,'hop')==0
+    parameter.hop=5;
+end
+if isfield(parameter,'numFeatures')==0
+    parameter.numFeatures=64;
+end
+if isfield(parameter,'deltaDelay')==0
+    parameter.deltaDelay=16;
+end
+if isfield(parameter,'precomputeCQT')==0
+    parameter.precomputeCQT = 0;
+end
+
+%% IO
+fid = fopen(reflist);
 curFileList = '';
 fileIndex = 1;
 curfile = fgetl(fid);
@@ -11,13 +41,22 @@ while ischar(curfile)
     fileIndex = fileIndex + 1;
 end
 
+%% merge data
 windowSize = 20;
+data = [];
 tic;
 for index = 1 : length(curFileList)
     curfile = curFileList{index};
     disp(['Generating data on #',num2str(index),': ',curfile]);
-    
-    
+    Q = computeQSpec(filename,parameter); % struct
+    logQ = preprocessQspec(Q);
+    for col = 1 : size(logQ, 2) - windowSize
+        sample = logQ(:, col : col + windowSize - 1);
+        data.cat(2, sample(:));
+    end
 end
+    
 toc
 fclose(fid);
+
+save(savename, 'data', '-v7.3');
