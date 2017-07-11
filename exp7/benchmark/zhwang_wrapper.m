46d5161fa10322e4652bf5db95b8060e83343a20
Haunter17 <haunterhacker17@gmail.com>
2017-07-11 16:18:08 -0700

    adding wrapper

--- exp7/benchmark/zhwang_wrapper.m
+++ exp7/benchmark/zhwang_wrapper.m
@@ -1,9 +1,15 @@
+%% param setup
 % param.fmin = ?;
 % param.fmax = ?;
 
+%% flag setup
 MINIFLAG = 0;
 TESTFLAG = 0;
 VALFLAG = 1;
 REPFLAG = 1;
 modelName = 'model';
 reportName = 'hp20';
+
+
+%% call driver
+benchmarkDriver
