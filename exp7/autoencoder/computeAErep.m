5d099c5ac37166c10bd7db6be4a4b6d2039088dd
Haunter17 <haunterhacker17@gmail.com>
2017-07-11 13:56:59 -0700

    tentative version for AE compute with mult layers

--- exp7/autoencoder/computeAErep.m
+++ exp7/autoencoder/computeAErep.m
@@ -1,4 +1,4 @@
-function F = computeAErep(Q, model, parameter)
+function F = computeAErep(spec, model, parameter)
 
 if nargin < 3
     parameter=[];
@@ -6,6 +6,11 @@ end
 
 W_list = model.W_list;
 b_list = model.b_list;
+num_layer = model.num_layer;
+
+if isfield(model, 'parameter')
+    parameter = model.parameter;
+end
 
 if model.num_layer == 1
     W_list = reshape(W_list, size(W_list, 2), size(W_list, 3));
@@ -25,24 +30,33 @@ if isfield(parameter,'numFeatures')==0
     parameter.numFeatures=size(b_list,2);
 end
 if isfield(parameter, 'useDelta')==0
-    parameter.delta = 0;
+    parameter.useDelta = 0;
 end
 if isfield(parameter,'deltaDelay')==0
     parameter.deltaDelay=16;
 end
 if isfield(parameter, 'medianThreshold')==0
-    parameter.threshold = 0;
+    parameter.medianThreshold = 0;
 end
 
 b_list = mat2cell(b_list, ones(num_layer, 1), parameter.numFeatures);
 
-spec = preprocessQspec(Q);
+% spec = preprocessQspec(Q);
 features = zeros(parameter.numFeatures, ceil((size(spec, 2) - parameter.m) / ...
     parameter.hop));
 for col = 1 : parameter.hop : size(spec, 2) - parameter.m
     X = spec(:, col : col + parameter.m - 1);
     X = X(:)';
-    f = X * W + b;
+    a = X;
+    for layer = 1 : num_layer - 1
+        W = W_list{layer};
+        b = b_list{layer};
+        a = poslin(a * W + b);
+    end
+    % do not take non-linearity at the last layer
+    W = W_list{num_layer};
+    b = b_list{num_layer};
+    f = a * W + b; 
     features(:, ceil(col / parameter.hop)) = f;    
 end
 
